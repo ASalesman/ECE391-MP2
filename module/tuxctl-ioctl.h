020f137ada80a85176db09d3c474156dace0a7fd
Sean Nicolay <kejistan@gmail.com>
2011-09-17 18:32:56 -0500

    Initial Provided Source

---
+++ module/tuxctl-ioctl.h
@@ -0,0 +1,9 @@
+
+#ifndef TUXCTL_H
+#define TUXCTL_H
+
+#define TUX_SET_LED _IOR('E', 0x10, unsigned long)
+#define TUX_READ_LED _IOW('E', 0x11, unsigned long*)
+#define TUX_BUTTONS _IOW('E', 0x12, unsigned long*)
+
+#endif
