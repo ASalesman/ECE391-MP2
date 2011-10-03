c28b89135e08cbf3b0753a5f9e98f59ba815ae1e
Sean Nicolay <kejistan@gmail.com>
2011-10-03 15:39:03 -0500

    fixed compile time errors resulting from incorrect use of octree

--- octree.c
+++ octree.c
@@ -2,14 +2,14 @@
 #include <assert.h>
 #include "octree.h"
 
-static struct octree_pair
+struct octree_pair
 {
 	size_t child_count;
 	size_t index;
 };
 
 /* lies about ordering to ensure sorted in descending order */
-static int octree_pair_compare(void *a, void *b)
+static int octree_pair_compare(void const *a, void const *b)
 {
 	/* subject to integer over/underflows */
 	return ((struct octree_pair *)b)->child_count -
@@ -55,10 +55,10 @@ void octree_insert(octree_node_t *octree, uint16_t pixel)
 	while (i < kOctreeSize) {
 		++level;
 		uint8_t child = octree_find_child_number(pixel, level);
-		octree[i]->red_total   += red;
-		octree[i]->green_total += green;
-		octree[i]->blue_total  += blue;
-		++octree[i]->child_total;
+		octree[i].red_total   += red;
+		octree[i].green_total += green;
+		octree[i].blue_total  += blue;
+		++octree[i].child_count;
 		i = octree_child_index(i, child);
 	}
 }
@@ -69,8 +69,9 @@ void octree_calculate_palette(uint8_t palette[192][3], octree_node_t *octree)
 	struct octree_pair pairs[kOctreeSize - kOctreeRowFourIndex];
 
 	for (i = kOctreeRowFourIndex; i < kOctreeSize; ++i) {
-		pairs[i - kOctreeRowFourIndex] = { octree[i]->child_count, i };
-		octree[i]->palette_index = 0; /* zero the index */
+		pairs[i - kOctreeRowFourIndex].child_count = octree[i].child_count;
+		pairs[i - kOctreeRowFourIndex].index = i;
+		octree[i].palette_index = 0; /* zero the index */
 	}
 
 	/* sort the octree 4th row entries according to child_count */
@@ -79,21 +80,29 @@ void octree_calculate_palette(uint8_t palette[192][3], octree_node_t *octree)
 
 	/* store the top colors in palette */
 	for (i = 0; i < 128; ++i) {
-		palette[i][0] = pairs[i]->red_total   / pairs[i]->child_count;
-		palette[i][1] = pairs[i]->green_total / pairs[i]->child_count;
-		palette[i][2] = pairs[i]->blue_total  / pairs[i]->child_count;
-		octree[pairs[i]->index]->palette_index = i;
+		octree_node_t *node = &octree[pairs[i].index];
+
+		/* check for divide by zero */
+		if (node->child_count) {
+			palette[i][0] = node->red_total   / node->child_count;
+			palette[i][1] = node->green_total / node->child_count;
+			palette[i][2] = node->blue_total  / node->child_count;
+		}
+		node->palette_index = i;
 	}
 
 	/* store the remaining average colors to the palette to ensure coverage */
 	for (i = 0; i < 64; ++i) {
-		octree_node_t *node = octree[i + kOctreeRowTwoIndex];
+		octree_node_t *node = &octree[i + kOctreeRowTwoIndex];
 		node->palette_index = i + 128;
 		octree_prune_children(octree, i + kOctreeRowTwoIndex);
-		size_t child_total = node->child_total;
-		palette[i + 128][0] = node->red_total   / child_total;
-		palette[i + 128][1] = node->blue_total  / child_total;
-		palette[i + 128][2] = node->green_total / child_total;
+
+		/* check for divide by zero */
+		if (node->child_count) {
+			palette[i + 128][0] = node->red_total   / node->child_count;
+			palette[i + 128][1] = node->blue_total  / node->child_count;
+			palette[i + 128][2] = node->green_total / node->child_count;
+		}
 	}
 }
 
@@ -104,31 +113,31 @@ void octree_prune_children(octree_node_t *octree, size_t index)
 	if (index >= kOctreeRowFourIndex) return;
 
 	/* clear the totals for this node */
-	octree[index]->child_count = 0;
-	octree[index]->red_total   = 0;
-	octree[index]->green_total = 0;
-	octree[index]->blue_total  = 0;
+	octree[index].child_count = 0;
+	octree[index].red_total   = 0;
+	octree[index].green_total = 0;
+	octree[index].blue_total  = 0;
 
 	for (i = 0; i < 8; ++i) {
 		size_t child_index = octree_child_index(index, i);
 		octree_prune_children(octree, child_index);
-		octree[index]->child_count += octree[child_index]->child_count;
-		octree[index]->red_total   += octree[child_index]->red_total;
-		octree[index]->green_total += octree[child_index]->green_total;
-		octree[index]->blue_total  += octree[child_index]->blue_total;
+		octree[index].child_count += octree[child_index].child_count;
+		octree[index].red_total   += octree[child_index].red_total;
+		octree[index].green_total += octree[child_index].green_total;
+		octree[index].blue_total  += octree[child_index].blue_total;
 	}
 }
 
 uint8_t octree_find_palette_index(octree_node_t *octree, uint16_t pixel)
 {
+	size_t index = 0;
 	uint8_t palette_index = 0;
 	uint8_t level;
-	size_t index = 0;
 
 	for (level = 0; level < 4; ++level) {
-		index = octree_find_child_index(index, octree_find_child_number(pixel, level));
-		if (octree[index]) {
-			palette_index = octree[index];
+		index = octree_child_index(index, octree_find_child_number(pixel, level));
+		if (octree[index].palette_index) {
+			palette_index = octree[index].palette_index;
 		}
 	}
 
